44e99c7421db6c68ee235bee01c8212f4f61ac17
Ruilinzhang0415 <472075013ruilinzhang@gmail.com>
2024-09-09 18:26:35 -0500

    Refacotr: minor update on spiral matrix IV.

--- src/solutions/spiral_matrix_iv.rs
+++ src/solutions/spiral_matrix_iv.rs
@@ -7,25 +7,28 @@ impl Solution {
         let n_col = n as usize;
         let mut result = vec![vec![-1; n_col]; n_row];
 
-        let mut curr_row: i32 = 0;
-        let mut curr_col: i32 = 0;
-        let mut dir_row: i32 = 0;
-        let mut dir_col: i32 = 1;
+        let mut curr_row = 0isize;
+        let mut curr_col = 0isize;
+        let directions = [(0, 1), (1, 0), (0, -1), (-1, 0),];
+        let mut dir_idx = 0;
 
         let mut curr_node = head.as_ref();
         while let Some(node) = curr_node {
             result[curr_row as usize][curr_col as usize] = node.val;
 
-            let (next_row, next_col) = (curr_row + dir_row, curr_col + dir_col);
-            if next_row < 0 || next_row as usize >= n_row ||
-               next_col < 0 || next_col as usize >= n_col ||
+            let next_row = curr_row + directions[dir_idx].0;
+            let next_col = curr_col + directions[dir_idx].1;
+
+            if next_row < 0 || next_row >= n_row as isize ||
+               next_col < 0 || next_col >= n_col as isize||
                result[next_row as usize][next_col as usize] != -1 {
-                let tmp = dir_row;
-                dir_row = dir_col;
-                dir_col = -tmp;
+                dir_idx = (dir_idx + 1) % 4;
+                curr_row = curr_row + directions[dir_idx].0;
+                curr_col = curr_col + directions[dir_idx].1;
+            } else {
+                curr_row = next_row;
+                curr_col = next_col;
             }
-            curr_row += dir_row;
-            curr_col += dir_col;
 
             curr_node = node.next.as_ref();
         }
